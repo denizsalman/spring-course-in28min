03bba43bc451a8819a66bab315fb80feaa76590a
Deniz Salman <ddenizsalman@gmail.com>
2023-01-26 21:55:43 +0300

    @Primary

--- src/main/java/com/example/in28minutesspring/QuickSortAlgorithm.java
+++ src/main/java/com/example/in28minutesspring/QuickSortAlgorithm.java
@@ -1,5 +1,10 @@
 package com.example.in28minutesspring;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
 public class QuickSortAlgorithm implements SortAlgorithm {
     @Override
     public int[] sort() {
